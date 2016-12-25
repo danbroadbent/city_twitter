4777db6fe1ccf1992461b066156ee59a16c6472e
Dan Broadbent <dbroadbent85@gmail.com>
2016-12-25 12:11:57 -0700

    GoogleService test passing

--- spec/service/gets_geolocation_from_google_request_spec.rb
+++ spec/service/gets_geolocation_from_google_request_spec.rb
@@ -3,10 +3,8 @@ require "rails_helper"
 describe "user query is turned to geolocation" do
   it "has a valid city" do
     location = 'Denver'
-    GoogleService.convert_to_coords(location)
+    coords = GoogleService.convert_to_coords(location)
 
-    expect(search['total']).to eq(17)
-    expect(search['stores'].count).to eq(15)
-    expect(search.class).to eq(Hash)
+    expect(coords).to eq("39.7392358,-104.990251")
   end
 end
